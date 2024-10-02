4d53222dd3f1d2fc21e7a2fee34ca9f2f084b934
Yana Ushakova <115149484+yana1982@users.noreply.github.com>
2024-10-02 19:42:07 +0300

    update server.js file

---
+++ src/db/models/contacts.js
@@ -0,0 +1,36 @@
+import { model, Schema } from 'mongoose';
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    isFavourite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'createdAt',
+      updatedAt: 'updatedAt',
+    },
+  },
+);
+
+export const ContactsCollection = model('contacts', contactsSchema);
