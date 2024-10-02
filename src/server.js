c56623f3550beb7af7b2a972db6bee0f50f731d1
Yana Ushakova <115149484+yana1982@users.noreply.github.com>
2024-10-02 14:24:26 +0300

    add files

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utils/env.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+  app.use(cors());
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+  app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World!',
+    });
+  });
+
+  app.use((req, res) => {
+    res.status(404).json({ message: 'Not found' });
+  });
+
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+};
