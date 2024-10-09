6ca9163e550316f19a85637669b5d3618e378557
Yana Ushakova <115149484+yana1982@users.noreply.github.com>
2024-10-09 22:28:30 +0300

    update

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,18 +1,15 @@
 import { getAllContacts, getContactById } from '../services/contacts.js';
+
 import createHttpError from 'http-errors';
 
 export const getContactsController = async (req, res, next) => {
-  try {
-    const contacts = await getAllContacts();
+  const contacts = await getAllContacts();
 
-    res.json({
-      status: '200',
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-  } catch (err) {
-    next(err);
-  }
+  res.json({
+    status: '200',
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
 };
 
 export const getContactByIdController = async (req, res) => {
@@ -20,10 +17,10 @@ export const getContactByIdController = async (req, res) => {
   const contact = await getContactById(contactId);
 
   if (!contact) {
-    throw createHttpError(404, `Contact not found`);
+    throw createHttpError(404, 'Student not found');
   }
 
-  res.status(200).json({
+  res.json({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
--- src/server.js
+++ src/server.js
@@ -34,46 +34,6 @@ export const setupServer = () => {
 
   app.use(errorHandler);
 
-  // app.use('*', (req, res, next) => {
-  //   res.status(404).json({
-  //     message: 'Not found',
-  //   });
-  // });
-
-  // app.use((err, req, res, next) => {
-  //   res.status(500).json({
-  //     message: 'Something went wrong',
-  //     error: err.message,
-  //   });
-  // });
-
-  // app.get('/contacts', async (req, res) => {
-  //   const contacts = await getAllContacts();
-
-  //   res.status(200).json({
-  //     status: '200',
-  //     message: 'Successfully found contacts!',
-  //     data: contacts,
-  //   });
-  // });
-
-  // app.get('/contacts/:contactId', async (req, res, next) => {
-  //   const { contactId } = req.params;
-  //   const contact = await getContactById(contactId);
-
-  //   if (!contact) {
-  //     return res.status(404).json({
-  //       message: `Contact not found`,
-  //     });
-  //   }
-
-  //   res.status(200).json({
-  //     status: 200,
-  //     message: `Successfully found contact with id ${contactId}!`,
-  //     data: contact,
-  //   });
-  // });
-
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
