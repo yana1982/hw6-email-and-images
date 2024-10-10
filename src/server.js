ed1b3e02d8724420d3c95b76c96cac635abe8bd2
Yana Ushakova <115149484+yana1982@users.noreply.github.com>
2024-10-10 15:58:38 +0300

    update files

--- src/server.js
+++ src/server.js
@@ -11,7 +11,12 @@ const PORT = Number(env('PORT', '3000'));
 export const setupServer = () => {
   const app = express();
 
-  app.use(express.json());
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
   app.use(cors());
 
   app.use(
